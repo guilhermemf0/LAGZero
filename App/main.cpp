f1fed5b26acef089f339bb9905a2f87f54f46b22
Guilherme Motta Franco <gui.mottafranco@gmail.com>
2025-08-10 19:26:52 -0300

    Inicio do App

--- App/main.cpp
+++ App/main.cpp
@@ -2,10 +2,23 @@
 
 #include <QApplication>
 
+#include <QSplashScreen>
+#include <QPixmap>
+#include <QThread>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPixmap pixmap(":/logo-site.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+
+    QThread::sleep(3);
+
     MainWindow w;
+    splash.finish(&w);
     w.show();
+
     return a.exec();
 }
